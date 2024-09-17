43b83337033e68ead6492dbc26a1efc8a56176ad
joannaz27 <joannaz27@nycstudents.net>
2024-09-17 09:10:42 -0400

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,6 +1,7 @@
-public static void main(String[] args)
-    {
-        Feeder f = new Feeder();
-        System.out.println(f);
-        System.out.println(f.currentFood);
+public class Main {
+    public static void main(String[] args){
+            Feeder f = new Feeder();
+            System.out.println(f);
+            System.out.println(f.currentFood);
+    }
 }
